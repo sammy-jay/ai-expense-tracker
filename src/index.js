5c2881139de273b9a96a8a741dfa144239e931fe
sammy-jay <sammyjayden27@gmail.com>
2022-03-14 20:08:26 +0100

    2 files modified

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import App from './App';
 import './index.css';
 
 ReactDOM.render(
-  <SpeechProvider appId="" language="en-US">
+  <SpeechProvider appId="f3017461-c1c5-40b8-9218-295cded2f607" language="en-US">
     <Provider>
       <App />
     </Provider>
